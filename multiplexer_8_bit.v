127589de9236f9993352a739d5e7eedbfd40150d
Matt DeRoos <MattDeRoos@u.boisestate.edu>
2026-03-25 19:43:13 -0600

    Add 8-bit multiplexer module

---
+++ multiplexer_8_bit.v
@@ -0,0 +1,12 @@
+module multplexer_8_bit (
+    input [7:0] A, B, C, D,
+  input [1:0] sel,
+    output [7:0] Out 
+);
+
+  assign Out = (sel == 2'b00 ? A : 
+                  sel == 2'b01 ? B : 
+                  sel == 2'b10 ? C : 
+                                 D); 
+
+endmodule
