3ac7dafcd92df04a5dbc761d0076b1afece4458a
Matt DeRoos <MattDeRoos@u.boisestate.edu>
2026-03-25 19:20:52 -0600

    Add D-Latch module with enable and data inputs

--- d_latch.v
+++ d_latch.v
@@ -1,1 +1,17 @@
+module d_latch(
+    input D, E,
+    output Q, NotQ
+);
+  
+// E - enable
+// D - Data
+  
+// Will contain D-Latch behavior
+always @(E, D) begin 
+  if (E)
+    Q <= D; 
+  end
 
+assign NotQ = ~Q;
+
+endmodule
